8ff7c3e9a66aa3cf7bdc41508c3a0435420962b4
aeruny <67850318+aeruny@users.noreply.github.com>
2022-11-04 23:28:17 -0500

    reformatted Student class

--- src/Student.java
+++ src/Student.java
@@ -2,28 +2,37 @@ import java.sql.*;
 import java.util.*;
 
 public class Student {
-  
-  private String name;
-  private ArrayList<Time> schedule;
-  private boolean included
-  
-  public Student(String newName, ArrayList<Time> newSchedule){
-  name=newName;
-  schedule=newSchedule;
-  included=true;
-  }
-    
-  public Student(String newName){
-  name=newName;
-  included=true;
-  }
-  
-  public ArrayList<Time> getSchedule(){return schedule;}
-  
-  public void setSchedule(ArrayList<Time> newSchedule){schedule = newSchedule;}
-  
-  public void toggleIncluded(){included = !included;}
-  
-  public boolean getIncluded(){return included;}
-  
+
+    private String name;
+    private ArrayList<Time> schedule;
+    private boolean included;
+
+    public Student(String name, ArrayList<Time> schedule) {
+        this.name = name;
+        this.schedule = schedule;
+        this.included = true;
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.schedule = new ArrayList<Time>();
+        this.included = true;
+    }
+
+    public ArrayList<Time> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(ArrayList<Time> newSchedule) {
+        schedule = newSchedule;
+    }
+
+    public void toggleIncluded() {
+        included = !included;
+    }
+
+    public boolean getIncluded() {
+        return included;
+    }
+
 }
