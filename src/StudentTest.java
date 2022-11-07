f4fc32db1331dbe905f8b09df559f54fa080985b
aeruny <67850318+aeruny@users.noreply.github.com>
2022-11-07 14:58:25 -0600

    added a test class for Student class (more tests will be added)

---
+++ src/StudentTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+        Student student = new Student("John Doe");
+        assertEquals("John Doe", student.getName());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+        Student student = new Student("John Doe");
+        student.setName("Jane Doe");
+        assertEquals("Jane Doe", student.getName());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSchedule() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSchedule() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toggleIncluded() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getIncluded() {
+    }
+}
